0cca54a631f099c86d8a56a98cd4169eb283cf62
everton-lacerda <everton_ml@outlook.com>
2019-09-18 19:16:02 -0300

    config db e add user

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 import express from 'express'
 import routes from './routes'
 
+import './database'
+
+
 class App {
     constructor() {
         this.server = express()
+        
         this.middleware()
-        this.routes();
+        this.routes()
     }
 
-    middleware() {
+    middleware(){
         this.server.use(express.json())
     }
-    routes() {
+    routes(){
         this.server.use(routes)
     }
 }
+
 export default new App().server
